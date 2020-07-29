828703b990e5150a0012f2f91bd2a97d260e9c3b
Alex Lewis <alexlewis374@gmail.com>
2020-07-29 17:07:35 +0100

    Fix bug in _isTenthFrameValid

--- src/tenthFrame.js
+++ src/tenthFrame.js
@@ -19,14 +19,19 @@ class TenthFrame extends Frame {
   }
 
   _isTenthFrameValid(roll1, roll2, roll3=0) {
+    console.log(roll1, roll2, roll3)
     if (this._frameScore(roll1, roll2, roll3) > MAX_TOTAL_SCORE) {
-      return false
+        console.log('1')
+        return false
+    } 
+    if (((roll1 + roll2) < MIN_FOR_THIRD_ROLL) && roll3 > 0) {
+        console.log('2')
+        return false
+    } 
+    if (roll1 > MAX_ROLL_SCORE || roll2 > MAX_ROLL_SCORE || roll3 > MAX_ROLL_SCORE) {
+        console.log('3')
+        return false
     }
-    if ((roll1 + roll2) < MIN_FOR_THIRD_ROLL && roll3 > 0) {
-      return false
-    }
-    if (roll1 || roll2 || roll3 > MAX_ROLL_SCORE) {
-      return false
-    }
+    console.log('4')
   }
 }
